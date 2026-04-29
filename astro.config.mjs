b29b24703f61716c16e8b68f0e94ee8d0a14f315
gul-284 <sg400317@gmail.com>
2026-04-30 04:13:47 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   // Replace 'gul-284' with your GitHub username
   site: 'https://gul-284.github.io', 
-  // Replace 'sara_portfolio.github' with your exact repo name
   base: '/portfolio',
 })
