6603ef24841f4f3b9cc8980915571dbb4baf7495
gul-284 <sg400317@gmail.com>
2026-04-30 04:24:53 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  // Replace 'gul-284' with your GitHub username
   site: 'https://gul-284.github.io', 
-  base: '/portfolio',
+  base: '/portfolio/', // Is slash ka aage aur peeche hona zaroori hai
 })
